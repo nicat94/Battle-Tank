fab96a83a9074e988a647d887f4841e75538a5a8
Nijat Nasibli <n.nasibli@box.az>
2017-01-11 18:24:29 +0400

    TankAIController found player tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,21 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller possesing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
